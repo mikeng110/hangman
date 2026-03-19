dd880e557f3b844b8dccc44945b5a5dc118ce883
Mikael Englund <mikael.englund110@gmail.com>
2026-03-19 13:52:24 +0100

    working version1
    get_all_pos returns positions of where a charadcter is found in the word
    We use it as quick way to reconize if any letters where found and if they where we easily insert them into the solution
    
    When we check if we reached the solution we use join to turn solution from array into a string
    
    We we exit the loop and have life left we survived, if not we died

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import os
+import random
+
 
 HANGMAN_PICS = [r'''
   +---+
@@ -54,17 +56,48 @@ HANGMAN_PICS = [r'''
 
 HANGMAN_WORDS = ["APPLE","CARROT","TELEPHONE", "CHOCOLATE","HARMONY", "BREATHE", "GOODBYE", "SHADOW", "WHISPER" ]
 
+def get_all_pos(c, s):
+    return [pos for pos, char in enumerate(s) if char == c]
+
+def print_text(text):
+    result = ""
+    for letter in text:
+        result += letter + " "
+    print(result)
 
 def game():
-    i = 0
-    while True: 
-        print("Lets play a game!")
+    MAX_FAIL = len(HANGMAN_PICS)-1
 
-        if i > 6: i = 0
+    i = 0
+    current_word = random.choice(HANGMAN_WORDS)
+    solution = len(current_word)*['_']
+
+    os.system('cls')
+    print(HANGMAN_PICS[i])
+    print_text(solution)
+     
+    while i < MAX_FAIL: 
+        letter= str(input("Guess a letter: ")).upper()
+        pos_found = get_all_pos(letter, current_word)
+        if len(pos_found) == 0:
+            i+=1
+        else:
+            for pos in pos_found:
+                solution[pos] = letter
+
         os.system('cls')
         print(HANGMAN_PICS[i])
-        
-        time.sleep(1)
-        i+=1
+        print_text(solution)
+
+        if ''.join(solution) == current_word:
+            break
+                
+    if i < MAX_FAIL:
+        print("\nCongratulations you won!")
+    else: 
+        print("\nYou are dead!")
+        print("The word was: "+ current_word)
 
 game()
+
+
